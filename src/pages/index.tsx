1b5f1763ca91a745eaef685233829c0a8374cc2b
alex <alex@me.com>
2023-03-26 08:05:12 -0700

    Added posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
         {!user.isSignedIn && <SignInButton/>}
         {!!user.isSignedIn && <SignOutButton/>}
       </div>
+        {data?.map((post) => <div key={post.id}>{post.content}</div>)}
       </main>
     </>
   );
