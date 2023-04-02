b8556c0bac349862dd9bf220b4fddcfd2136ccef
alex <alex@me.com>
2023-04-01 21:11:33 -0700

    Fix signin, woops!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            {isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 <SignInButton />
               </div>
