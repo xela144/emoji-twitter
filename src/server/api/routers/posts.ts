097e9584975705c56c5ebef84592c519f2a66390
alex <alex@me.com>
2023-04-04 22:46:51 -0700

    trcp posts mutation: emojis

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/dist/api";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter as router,
+  publicProcedure as procedure,
+  privateProcedure,
+} from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
@@ -13,10 +17,11 @@ const filterUserForClient = (user: User) => {
   };
 };
 
-export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
+export const postsRouter = router({
+  getAll: procedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
 
     const users = (
@@ -43,4 +48,21 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }), // getAll
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+      return post;
+    }),
 });
