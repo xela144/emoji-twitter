b61690d6d272cdf30e3d655c6de898b0ba635425
alex <alex@me.com>
2023-04-08 08:42:23 -0700

    Manage signin/signout with "UserButton"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,12 +40,15 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
       />
       <input
         placeholder="Type some emojis"
