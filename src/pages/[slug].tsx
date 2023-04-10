8052904edb3c9fc56e2e685ff70a8e8d51f86da9
alex <alex@me.com>
2023-04-09 21:38:35 -0700

    Profile page with SSG helper

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { PageLayout } from "~/components/layout";
 import { api } from "~/utils/api";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
@@ -22,7 +23,18 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{title}</title>
       </Head>
       <PageLayout>
-        <div>{`@${data.username}`}</div>
+        <div className="relative h-40 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? "user"}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 -mb-16 ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
